8596e8a7a8e226ac351fd0e729d9217e9ee81d6a
marcosvandrade <dcemvasconcellos@dgp.local>
2019-06-14 11:42:16 -0300

    uploadWork14Jun19

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function trocarDiv(nome, idade){
     var texto = prompt("Qual o seu sobrenome?");
     area.innerHTML = nome+" "+texto+" tem "+idade+" anos";
 }
+
+function  adicionarIngrediente() {
+    var ing = document.getElementById("ingrediente").value;
+
+    var listahtml = document.getElementById("lista").innerHTML;
+
+    listahtml = listahtml + "<li>" + ing + "</li>";
+
+    document.getElementById("lista").innerHTML = listahtml;
+
+  }
